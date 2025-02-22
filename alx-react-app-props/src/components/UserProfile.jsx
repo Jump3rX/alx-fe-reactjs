7fc1375bc9d41f0c2719eef04bca6a252b4e0991
conrad <conradmax5@gmail.com>
2025-02-22 13:21:05 +0300

    props

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { UserContext } from "../UserContext";
+import { useContext } from "react";
+
+function UserProfile(props) {
+  const userData = useContext(UserContext);
+  return (
+    <div>
+      <h2>{userData.name}</h2>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
