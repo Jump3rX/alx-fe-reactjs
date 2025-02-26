35d9664d5f0f8128331c90059dd0b04be02685eb
conrad <conradmax5@gmail.com>
2025-02-26 22:25:43 +0300

    Zustand

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+function App() {
+  return (
+    <>
+      <AddRecipeForm />
+      <RecipeList />
+    </>
+  );
+}
+
+export default App;
