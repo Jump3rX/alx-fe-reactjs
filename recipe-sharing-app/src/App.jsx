48baf8d298f1c82580fa170b277d896835d20b7f
conrad <conradmax5@gmail.com>
2025-02-27 22:27:50 +0300

    edit and delete features

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from "react";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 function App() {
   return (
     <>
-      <AddRecipeForm />
-      <RecipeList />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipes/:recipeId" element={<RecipeDetailsWrapper />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
+const RecipeDetailsWrapper = () => {
+  const { recipeId } = useParams();
+  return <RecipeDetails recipeId={Number(recipeId)} />;
+};
 export default App;
---
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import useRecipeStore from "./recipeStore";
+import { useNavigate } from "react-router-dom";
+function DeleteRecipeButton({ recipeId }) {
+  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate();
+  const handleDelete = () => {
+    if (window.confirm("Are you sure you want to delete this recipe?")) {
+      deleteRecipe(recipeId);
+      navigate("/");
+    }
+  };
+  return (
+    <div>
+      <button onClick={handleDelete}>Delete Recipe</button>
+    </div>
+  );
+}
+
+export default DeleteRecipeButton;
