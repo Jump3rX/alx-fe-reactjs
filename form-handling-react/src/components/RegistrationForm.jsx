f8a0fae3598e2c442db777d99d4d70ead579b697
conrad <conradmax5@gmail.com>
2025-03-05 22:34:10 +0300

    form handling new fix

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -5,12 +5,23 @@ function RegistrationForm() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
 
+  function validateForm() {
+    let newErrors = {};
+    if (!username) newErrors.username = "Username is required";
+    if (!email) {
+      newErrors.email = "Email is required";
+    }
+    if (!password) {
+      newErrors.password = "Password is required";
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  }
   function handleSubmit(e) {
-    if (!username || !email || !password) {
-      alert("All fields must be completed!");
-    } else {
-      console.log(formData);
+    if (validateForm()) {
+      console.log("submitted");
     }
   }
 
@@ -26,6 +37,7 @@ function RegistrationForm() {
           value={username}
           required
         />
+        {errors.username && <small>{errors.username}</small>}
 
         <label htmlFor="email"></label>
         <input
@@ -36,6 +48,7 @@ function RegistrationForm() {
           value={email}
           required
         />
+        {errors.email && <small>{errors.email}</small>}
 
         <label htmlFor="password"></label>
         <input
@@ -46,7 +59,7 @@ function RegistrationForm() {
           value={password}
           required
         />
-
+        {errors.password && <small>{errors.password}</small>}
         <button type="submit">Register</button>
       </form>
     </div>
