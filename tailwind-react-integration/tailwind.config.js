d7cc135efb7eab48fb0e61295a141f052884a5c1
conrad <conradmax5@gmail.com>
2025-03-14 09:45:34 +0300

    tailwind test

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  content: ["./src/**/*.{html,js,tsx,jsx}"],
+  theme: {
+    extend: {},
+  },
+  variants: { extend: {} },
+  plugins: [],
+};
