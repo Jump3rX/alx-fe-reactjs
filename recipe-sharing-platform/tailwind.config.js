94f1a07b9b3bd50febc20431dbd6ce4883072931
conrad <conradmax5@gmail.com>
2025-03-17 22:55:47 +0300

    initial tailwind setup

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,tsx,jsx}", "./public/index.html"], // Updated to only use content
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: { extend: {} },
+  plugins: [],
+};
