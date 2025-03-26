93f2148b573e7d3017626e23107689770ac46640
conrad <conradmax5@gmail.com>
2025-03-26 23:00:56 +0300

    user search integration and testing done

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,15 +1,48 @@
 import React from "react";
-
+import { useState } from "react";
+import { fetchUserData } from "../services/githubService";
 function Search() {
+  const [username, setUsername] = useState("");
+  const [userData, setUserData] = useState(null);
+
+  function handleSetUsername(e) {
+    setUsername(e.target.value);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let userInfo = await fetchUserData(username);
+    console.log(userInfo);
+    setUserData(userInfo);
+    setUsername("");
+  }
   return (
     <div>
       <h2>Search a GitHub user</h2>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="username">Enter Username</label>
-        <input type="text" name="username" id="username" />
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={username}
+          onChange={(e) => handleSetUsername(e)}
+        />
         <button type="submit">Search</button>
       </form>
+
+      {userData ? (
+        <div className="card">
+          <img src={userData.avatar_url} alt="" />
+          <h3>Name: {userData.name}</h3>
+          <a href={userData.html_url} target="_blank">
+            Profile Link
+          </a>
+        </div>
+      ) : (
+        <h2>No user data available!</h2>
+      )}
     </div>
   );
 }
--- github-user-search/src/services/githubServices.js
+++
@@ -1,3 +0,0 @@
-import axios from "axios";
-const key = import.meta.env.VITE_APP_GITHUB_API_KEY;
-const baseURL = "https://api.github.com/users/";
